eaeeef948f4833d70873074ebbc993facfd2fb44
moreih29 <moreih29@gmail.com>
2023-04-18 21:21:00 +0900

    fix: setup requires 추가

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 
-import setuptools
+from setuptools import setup, find_packages
 
-
-def get_version():
-    with open('vismo/version.py') as f:
-        exec(f.read())
-    return locals()['__version__']
+from vismo import __version__
 
 
 def get_readme():
@@ -13,27 +9,39 @@ def get_readme():
         return f.read()
 
 
-setuptools.setup(
-    name='vismo',
-    version=get_version(),
-    license='MIT',
-    author='moreih29',
-    author_email='moreih29@gmail.com',
-    description='Pytorch Vision Models',
-    long_description=get_readme(),
-    packages=setuptools.find_packages(),
-    url='https://github.com/moreih29/vismo',
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    keywords='pytorch vision model classification object detection segmentation pose estimation',
-    include_package_data=True,
-    install_requires=[],
-    python_requires='>=3.7',
-)
+def get_requires():
+    requires = [
+        'opencv-python>=4.5',
+    ]
+    return requires
+
+
+def main():
+    setup(
+        name='vismo',
+        version=__version__,
+        license='MIT',
+        author='moreih29',
+        author_email='moreih29@gmail.com',
+        description='Pytorch Vision Models',
+        long_description=get_readme(),
+        packages=find_packages(),
+        url='https://github.com/moreih29/vismo',
+        classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.8',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+            'Topic :: Software Development',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+        keywords='pytorch vision model classification object detection segmentation pose estimation',
+        include_package_data=True,
+        install_requires=get_requires(),
+        python_requires='>=3.7',
+    )
+    
+
+if __name__ == '__main__':
+    main()
